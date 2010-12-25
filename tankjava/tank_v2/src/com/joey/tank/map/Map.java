d8f517613e581c6436dd870546952d3d27cdf525
zy0422_lei@hotmail.com <zy0422_lei@hotmail.com@029b91e9-8802-2db4-aed3-d150fa5a69b1>
2010-12-25 07:49:59 +0000

    update

--- tankjava/tank_v2/src/com/joey/tank/map/Map.java
+++ tankjava/tank_v2/src/com/joey/tank/map/Map.java
@@ -1,8 +1,14 @@
 package com.joey.tank.map;
 
+import java.awt.Graphics;
+
+import com.joey.tank.beans.StaticElement;
+
 public class Map {
 
 	private int[][] models;
+
+	private StaticElement[][] elements;
 
 	public int[][] getModels() {
 		return models;
@@ -12,4 +18,15 @@ public class Map {
 		this.models = models;
 	}
 
+	public StaticElement[][] getElements() {
+		return elements;
+	}
+
+	public void setElements(StaticElement[][] elements) {
+		this.elements = elements;
+	}
+
+	public void draw(Graphics g) {
+		
+	}
 }
