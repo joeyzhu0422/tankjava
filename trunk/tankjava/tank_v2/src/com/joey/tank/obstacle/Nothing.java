a9a9a237ad3adcc171ae906242017579895e4103
zy0422_lei@hotmail.com <zy0422_lei@hotmail.com@029b91e9-8802-2db4-aed3-d150fa5a69b1>
2010-12-25 11:14:22 +0000

    update

---
+++ trunk/tankjava/tank_v2/src/com/joey/tank/obstacle/Nothing.java
@@ -0,0 +1,17 @@
+package com.joey.tank.beans.obstacle;
+
+import java.awt.Graphics;
+
+import com.joey.tank.constant.Constant;
+
+public class Nothing extends Obstacle {
+
+	public Nothing(int x, int y) {
+
+		super(x, y);
+		this.color = Constant.Scene.SCENE_COLOR;
+
+	}
+
+	
+}
---
+++ trunk/tankjava/tank_v2/src/com/joey/tank/obstacle/Obstacle.java
@@ -0,0 +1,24 @@
+package com.joey.tank.beans.obstacle;
+
+import java.awt.Graphics;
+
+import com.joey.tank.beans.StaticElement;
+import com.joey.tank.constant.Constant;
+
+public abstract class Obstacle extends StaticElement {
+
+	public Obstacle(int x, int y) {
+		this.width = Constant.Scene.CELL_LENGTH;
+		this.height = Constant.Scene.CELL_LENGTH;
+		this.x = x;
+		this.y = y;
+	}
+
+	public void draw(Graphics g) {
+
+		g.setColor(this.color);
+		g.fill3DRect(x * Constant.Scene.CELL_LENGTH, y
+				* Constant.Scene.CELL_LENGTH, width, height, true);
+
+	}
+}
