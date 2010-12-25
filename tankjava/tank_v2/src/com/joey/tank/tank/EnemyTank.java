4abe5d721e5a5eb4cf88cea5d178957842032e2b
zy0422_lei@hotmail.com <zy0422_lei@hotmail.com@029b91e9-8802-2db4-aed3-d150fa5a69b1>
2010-12-25 11:12:27 +0000

    update

---
+++ tankjava/tank_v2/src/com/joey/tank/tank/EnemyTank.java
@@ -0,0 +1,10 @@
+package com.joey.tank.beans.tank;
+
+
+public class EnemyTank extends BaseAbstractTank {
+
+	public void action() throws Exception {
+		System.out.println("Enemy Tank is action");
+	}
+
+}
