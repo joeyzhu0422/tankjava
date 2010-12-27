765b94f16ad6156c5b577bdee51ccec3d13ac368
joeyzhu0422 <joeyzhu0422@029b91e9-8802-2db4-aed3-d150fa5a69b1>
2010-12-27 02:19:22 +0000

    upadte

--- trunk/tankjava/tank_v2/src/com/joey/tank/listener/impl/BulletMoveListenerImpl.java
+++ trunk/tankjava/tank_v2/src/com/joey/tank/listener/impl/BulletMoveListenerImpl.java
@@ -1,21 +1,12 @@
 package com.joey.tank.listener.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.joey.tank.beans.ActiviteElement;
-import com.joey.tank.beans.Bullet;
-import com.joey.tank.beans.obstacle.Obstacle;
-import com.joey.tank.constant.Constant;
 import com.joey.tank.listener.MoveListener;
-import com.joey.tank.map.Map;
-import com.joey.tank.map.MapLoader;
-import com.joey.tank.util.MapUtil;
 
 public class BulletMoveListenerImpl implements MoveListener {
 
 	public boolean isCollide(ActiviteElement element) {
-		System.out.println("Tank Move Listener isCollide");
+		System.out.println("Bullet Move Listener isCollide");
 
 		return true;
 	}
