4e68ac7caefdc819a96127d94d8d8c3daaee4ad5
joeyzhu0422@gmail.com <joeyzhu0422@gmail.com@029b91e9-8802-2db4-aed3-d150fa5a69b1>
2011-07-12 09:34:04 +0000

    update

--- trunk/pigShoot/src/info/cloverapp/game/pig/scene/IScene.java
+++ trunk/pigShoot/src/info/cloverapp/game/pig/scene/IScene.java
@@ -4,16 +4,46 @@ import java.awt.Graphics;
 import java.awt.event.KeyListener;
 import java.util.List;
 
+/**
+ * 
+ * 场景接口
+ * 
+ * @author joey
+ *
+ */
 public interface IScene {
 
+	/**
+	 * 初始化方法
+	 */
 	public void init();
 
+	/**
+	 * 
+	 */
 	public void action();
 
+	/**
+	 * 描绘方法
+	 * 
+	 * @param g
+	 * @param width
+	 * @param height
+	 */
 	public void draw(Graphics g, int width, int height);
 
+	/**
+	 * 键盘监听器获取
+	 * 
+	 * @return
+	 */
 	public List<KeyListener> getKeyListenerList();
 
+	/**
+	 * 是否在跑
+	 * 
+	 * @return
+	 */
 	public boolean isRun();
 
 }
