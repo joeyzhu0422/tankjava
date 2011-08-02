3871527999fa574dac73e97e57be68211dcfe9da
joeyzhu0422@gmail.com <joeyzhu0422@gmail.com@029b91e9-8802-2db4-aed3-d150fa5a69b1>
2011-08-02 13:58:57 +0000

    update

---
+++ trunk/pigShoot/src/info/cloverapp/game/pig/container/GameContainer.java
@@ -0,0 +1,23 @@
+package info.cloverapp.game.pig.container;
+
+import info.cloverapp.game.pig.beans.Pig;
+import info.cloverapp.game.pig.beans.Wolf;
+
+import java.util.List;
+
+
+public class GameContainer {
+	
+	private Pig pig;
+	
+	private List<Wolf> wolfs;
+
+	public Pig getPig() {
+		return null;
+	}
+	
+	public List<Wolf> getWolfs() {
+		return null;
+	}
+	
+}
